c0862a6efb7c5f0d77c50e36611452e380f88709
Veeresh Kamble <veereshkamble7@gmail.com>
2018-05-07 18:13:11 -0700

    adding show labels method in app.py and new test.py

--- 295/LearnAR/darkflow-master_final/darkflow-master/test.py
+++ 295/LearnAR/darkflow-master_final/darkflow-master/test.py
@@ -2,13 +2,14 @@ import cv2
 import numpy as np
 from darkflow.net.build import TFNet
 import matplotlib.pyplot as plt
+import json
 
 #%config InlineBackend.figure_format = 'svg'
 
 options = {
   'model': 'cfg/tiny-yolo-voc-4c.cfg',
-  'load': 107500,
-  'threshold': 0.2
+  'load': 111375,
+  'threshold': 0.1
 }
 
 tfnet = TFNet(options)
@@ -25,11 +26,19 @@ for i in range (0,len(result)):
 
     img = cv2.rectangle(img, tl, br, (0, 255, 0), 7)
     img = cv2.putText(img, label, tl, cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 2)
-    cv2.imwrite('label_img/circuit_label.jpg', img)
-    res = jsonify(result)
 
-    with open('sample_img/out/circuit.json', 'w') as fh:
-        fh.write(res)
+cv2.imwrite('label_img/circuit_label.jpg', img)
+for i in range(len(result)):
+    # for key, value in result[i].items():
+    del result[i]['confidence']
+        # if type(value) is np.float32:
+        #     value = value.item()
+        #     print(type(value))
+print (result)
+res = json.dumps(result)
+
+with open('sample_img/out/circuit.json', 'w') as fh:
+    fh.write(res)
 
 # plt.imshow(img)
 # plt.show()
